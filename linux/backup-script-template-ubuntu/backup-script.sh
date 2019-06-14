76566610e628d551597dc969fc86622ce4c78881
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2019-06-14 20:58:02 -0300

    update bkp script linux

--- linux/backup-script-template-ubuntu/backup-script.sh
+++ linux/backup-script-template-ubuntu/backup-script.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "----------------------START----------------------" >> logfile
 echo "$(date) # # # Moving files" >> logfile
-cp -Ruv source-dir/* target-dir >> logfile
+cp --recursive --update --verbose source-dir/* target-dir >> logfile
 echo "$(date) # # # Complete!" >> logfile
 echo "-----------------------END-----------------------" >> logfile
+echo "\n\n" >> logfile
 echo "End backup -> check logfile"
 # end code
 
