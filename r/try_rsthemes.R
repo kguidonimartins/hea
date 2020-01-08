0c8a24952e8e49bac556a08868a907f80c8ae939
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2020-01-08 00:26:29 -0300

    add try new rstudio themes

---
+++ r/try_rsthemes.R
@@ -0,0 +1,13 @@
+devtools::install_github("gadenbuie/rsthemes")
+
+# install all the themes
+rsthemes::install_rsthemes()
+
+# list installed themes
+rsthemes::list_rsthemes()
+
+# Try just the light, dark, or base16 themes, e.g.
+rsthemes::try_rsthemes()
+
+# remove 
+rsthemes::remove_rsthemes()
